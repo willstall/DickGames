a376544caab55b10fc9324051a797c7b9adfb013
Will Stallwood <will@cipherprime.com>
2017-05-29 15:35:44 -0400

    spring

--- js/main.js
+++ js/main.js
@@ -9,11 +9,13 @@ function main()
 	// Display Test
 	var testing = new createjs.Shape();
 		//testing.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-		testing.graphics.beginFill("DeepSkyBlue").rect(0,0,50,50);
+		testing.graphics.beginFill("DeepSkyBlue").rect(-25,-25,50,50);
 		testing.originX = testing.x;
-		testing.counter = 0;
-		testing.increment = .1;
-		testing.amplitude = 50;
+		testing.xp = testing.x;
+		testing.yp = testing.y;
+		// testing.counter = 0;
+		// testing.increment = .1;
+		// testing.amplitude = 50;
 		testing.on("tick", update);
 
 	container.addChild(testing);  
@@ -34,6 +36,57 @@ function keyPressed( event )
 
 function update( event )
 {
-	event.target.x = event.target.originX + Math.sin( event.target.counter ) * event.target.amplitude;
-	event.target.counter += event.target.increment;
+	var inertia = 0.9 ;
+	var k = .3 ;
+
+	var target = event.target ;
+
+	var mp = target.parent.globalToLocal( stage.mouseX , stage.mouseY ) ;
+	var x = -target.x + mp.x ;
+	var y = -target.y + mp.y ;
+
+	target.xp = target.xp * inertia + x*k ;
+	target.yp = target.yp * inertia + y*k ;
+
+	target.x += target.xp ;
+	target.y += target.yp ;
 }
+
+// function update( event )
+// {
+// 	event.target.x = event.target.originX + Math.sin( event.target.counter ) * event.target.amplitude;
+// 	event.target.counter += event.target.increment;
+// }
+
+
+/*
+
+onClipEvent (load) {
+	// inertia relates to the quantity of energy that 
+	// the spring will carry
+	// inertia = 1 would mean that the spring doesn't 
+	// loose any energy, and that it will oscillate
+	// forever
+	inertia = 0.9 ;
+
+	// k relates to the spring, and how "hard" it will be.
+	// The higher k the faster the mass will come back.
+	k = 0.1 ;
+}
+
+onClipEvent (enterFrame) {
+
+	// We calculate the distance to the mouse
+	x = -this._x + _root._xmouse ;
+	y = -this._y + _root._ymouse ;
+	
+	//We calculate the amount by which the mass will to move
+	xp = xp * inertia + x*k ;
+	yp = yp * inertia + y*k ;
+
+	//We move it
+	_x += xp ;
+	_y += yp ;
+}
+
+*/
