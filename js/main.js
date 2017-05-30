deee9b09cc16fd06eafa12e57e853c41acb11645
Will Stallwood <will@cipherprime.com>
2017-05-29 20:38:03 -0400

    multiple colors

--- js/main.js
+++ js/main.js
@@ -5,96 +5,30 @@ function main()
 {	
 	// Setup
 	setup();
-	
-	// Keyboard Test
-	document.onkeydown = keyPressed;
 
-	// Display Test
-	var testing = new createjs.Shape();
-		//testing.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-		testing.graphics.beginFill("DeepSkyBlue").rect(-25,-25,50,50);
-		testing.originX = testing.x;
-		testing.xp = testing.x;
-		testing.yp = testing.y;
-		testing.alpha = 0;
-		// testing.counter = 0;
-		// testing.increment = .1;
-		// testing.amplitude = 50;
-		testing.on("tick", update);
+	// Project
+	var range = 100;
+	var colors = [
+		{r : 0, g : 0, b: 0,a: 1},
+		{r : 255, g : 255, b: 0,a: 1},
+		{r : 0, g : 255, b: 255,a: 1},
+		{r : 255, g : 0, b: 255,a: 1}
+	]
 
-	spring = new Spring();
-	spring.inertia = 0.92;
-	spring.k = 0.6;
-
-
-	trail = new Trail();
-	trail.targetX = testing.x;
-	trail.targetY = testing.y;
-
-	container.addChild(spring,trail,testing);  
-
-	// Extension Test
-  	var extend_test = new ExtendedContainer();
-		extend_test.output();
-}
-	
-function keyPressed( event )
-{
-	//Keycodes found at http://keycode.info
-	if( event.keyCode == 32 )
+	for(var i = 1; i < 4; i ++)
 	{
-		console.log("testing");
+		var springTrail = new SpringTrail();
+			springTrail.x = ( Math.random() - .5 ) * range;
+			springTrail.y = ( Math.random() - .5 ) * range;
+			springTrail.trail.color = colors[i];
+			springTrail.spring.k = Math.random();
+			springTrail.spring.interia = Math.random();
+
+		container.addChild( springTrail );
 	}
+
+	var initialTrail = new SpringTrail();
+		initialTrail.trail.color = colors[0];
+
+	container.addChild( initialTrail );	
 }
-
-function update( event )
-{
-	var mp = spring.parent.globalToLocal( stage.mouseX , stage.mouseY ) ;
-
-	spring.targetX = mp.x;
-	spring.targetY = mp.y;
-	spring.update();
-
-	trail.targetX = spring.x;
-	trail.targetY = spring.y;
-	trail.update();
-}
-
-// function update( event )
-// {
-// 	event.target.x = event.target.originX + Math.sin( event.target.counter ) * event.target.amplitude;
-// 	event.target.counter += event.target.increment;
-// }
-
-
-/*
-
-onClipEvent (load) {
-	// inertia relates to the quantity of energy that 
-	// the spring will carry
-	// inertia = 1 would mean that the spring doesn't 
-	// loose any energy, and that it will oscillate
-	// forever
-	inertia = 0.9 ;
-
-	// k relates to the spring, and how "hard" it will be.
-	// The higher k the faster the mass will come back.
-	k = 0.1 ;
-}
-
-onClipEvent (enterFrame) {
-
-	// We calculate the distance to the mouse
-	x = -this._x + _root._xmouse ;
-	y = -this._y + _root._ymouse ;
-	
-	//We calculate the amount by which the mass will to move
-	xp = xp * inertia + x*k ;
-	yp = yp * inertia + y*k ;
-
-	//We move it
-	_x += xp ;
-	_y += yp ;
-}
-
-*/
